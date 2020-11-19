323a6f9e65bbedb67272b167f29d7d2721328004
Quoding <46576765+Quoding@users.noreply.github.com>
2020-11-18 19:45:13 -0500

    Fix output.jpg filename bug.
    Patch for unreliable filenames - now actually sort by dates instead of fetching only last element.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from os.path import exists
 from os import mkdir
 from time import sleep
+from datetime import datetime
 import ctypes
 from win32com.shell import shell, shellcon
 from requests import get
@@ -11,6 +12,16 @@ url = "https://deadauthor.org/art/"
 image_path = ""
 PICTURES_FOLDER = shell.SHGetFolderPath(0, shellcon.CSIDL_MYPICTURES, None, 0)
 DIR = r"{}\abstrakt_images".format(PICTURES_FOLDER)
+
+
+def get_dates(datelines):
+    dates = [
+        " ".join(date.strip().split(" ")[0:2]) for date in datelines
+    ]  # Extract just the motherfucking date from this shitty ass format
+
+    dates = [datetime.strptime(date, "%d-%b-%Y %H:%M") for date in dates]
+
+    return dates
 
 
 def download_image(url, path):
@@ -25,8 +36,16 @@ def get_latest_image(url):
 
     html_text = get(url).text
     soup = BeautifulSoup(html_text, "html.parser")
-    images = soup.findAll("a", href=True)
-    last_image_name = images[-1]["href"]
+    lines = soup.find("pre").contents
+    images = lines[2::2]
+    datelines = lines[3::2]
+
+    dates = get_dates(datelines)
+
+    image_dates = list(zip(images, dates))
+    image_dates = sorted(image_dates, key=lambda t: t[1], reverse=True)
+
+    last_image_name = image_dates[0][0].get_text()
 
     image_url = url + last_image_name
     path = DIR + "/" + last_image_name
